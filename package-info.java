c1ff8f3cf1322aabdd0aa2f4ae7cf5816f3a9029
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-28 16:00:43 +0000

    Done java DiceGame

---
+++ package-info.java
@@ -0,0 +1,11 @@
+/**.
+* This is the package info file for the Java Dice Game program.
+*
+* This program will check if the user guessed the correct number
+* Between 1 and 6 with a random number generator and display
+* If the number is higher or lower
+*
+* @author  Remy Skelton
+* @version 1.0
+* @since   2025-February-26
+*/
